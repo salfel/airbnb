026620bb6e7271e2d9a95f96c376393e9ee5e687
Felix Salcher <dev.felix.salcher@gmail.com>
2023-12-27 16:04:53 +0100

    refactor: removed unused imports

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -1,16 +1,7 @@
 import { Apartment, PageProps, Pagination } from "@/types";
 import AuthenticatedLayout from "@/layouts/AuthenticatedLayout";
-import { Head, Link } from "@inertiajs/react";
+import { Head } from "@inertiajs/react";
 import React, { ReactNode } from "react";
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { formatDateRange } from "@/lib/utils";
 import Paginator from "@/components/Paginator";
 import ApartmentPreview from "@/components/ApartmentPreview";
 
@@ -22,7 +13,7 @@ export default function Home({
             <Head title="Home" />
 
             <div className="grid sm:grid-cols-3 grid-cols-2 gap-12">
-                {apartments.data.map((apartment, index) => (
+                {apartments.data.map((apartment) => (
                     <ApartmentPreview apartment={apartment} />
                 ))}
             </div>
