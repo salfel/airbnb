dd10ba1094c2c752ff5d56e09188d3727a4e8c41
Felix Salcher <dev.felix.salcher@gmail.com>
2023-12-27 22:32:45 +0100

    feat: added review table and model

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Apartment extends Model
 {
@@ -34,5 +35,10 @@ class Apartment extends Model
     public function host(): BelongsTo
     {
         return $this->belongsTo(Host::class);
+    }
+
+    public function reviews(): HasMany
+    {
+        return $this->hasMany(Review::class);
     }
 }
---
+++ app/Models/Review.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Review extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'stars',
+        'comment',
+        'user_id',
+        'apartment_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function apartment(): BelongsTo
+    {
+        return $this->belongsTo(Apartment::class);
+    }
+}
