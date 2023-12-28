5ce27bf3b663122f36289bb4b65ba413fa271d64
Felix Salcher <dev.felix.salcher@gmail.com>
2023-12-29 00:24:11 +0100

    fix: added key property

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -14,7 +14,10 @@ export default function Home({
 
             <div className="grid sm:grid-cols-3 grid-cols-2 gap-12">
                 {apartments.data.map((apartment) => (
-                    <ApartmentPreview apartment={apartment} />
+                    <ApartmentPreview
+                        apartment={apartment}
+                        key={apartment.id}
+                    />
                 ))}
             </div>
 
