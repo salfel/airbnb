f31062633dafa6b1b5954ca2c45db747c885bca2
Felix Salcher <dev.felix.salcher@gmail.com>
2023-12-29 14:02:38 +0100

    revert: removed profile routes

--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,4 @@ Route::resource('apartments.reviews', ReviewController::class)->except(['show'])
 Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 require __DIR__ . '/auth.php';
