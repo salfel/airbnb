d4185da6636b8cbfea65ef58a2246a48e44e7129
Felix Salcher <dev.felix.salcher@gmail.com>
2023-12-30 01:03:29 +0100

    feat: created general structure of create and created CountryInput

--- resources/js/Pages/Apartment/Create.tsx
+++ resources/js/Pages/Apartment/Create.tsx
@@ -1,8 +1,91 @@
+import React from "react";
 import { ReactNode } from "react";
 import AuthenticatedLayout from "@/layouts/AuthenticatedLayout";
+import { Head, router, usePage } from "@inertiajs/react";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import FormInput from "@/components/FormInput";
+import CountryInput, {
+    FormValues,
+} from "@/Pages/Apartment/Create/CountryInput";
+import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
+import { useForm } from "react-hook-form";
 
 export default function Create() {
-    return <div></div>;
+    const {
+        props: { errors },
+    } = usePage();
+    const form = useForm<FormValues>({
+        defaultValues: {
+            title: "",
+            description: "",
+            price: 0,
+            city: "",
+            country: "",
+            beds: 0,
+            baths: 0,
+            start: "",
+            end: "",
+            attributes: [],
+            images: [],
+        },
+    });
+
+    function handleSubmit(values: FormValues) {
+        router.post(route("apartments.store"), values);
+    }
+
+    return (
+        <>
+            <Head title="Start renting" />
+
+            <Card>
+                <CardHeader>
+                    <CardTitle>Start renting your first apartment</CardTitle>
+                    <CardDescription>
+                        This form allows you to list your first apartment on our
+                        platform. Please provide as much detail as possible to
+                        attract potential renters.
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Form {...form}>
+                        <form
+                            onSubmit={form.handleSubmit(handleSubmit)}
+                            className="space-y-6"
+                        >
+                            <FormInput
+                                name="title"
+                                control={form.control}
+                                error={errors.title}
+                            />
+
+                            <FormInput
+                                name="description"
+                                control={form.control}
+                                error={errors.description}
+                            />
+
+                            <FormInput
+                                name="country"
+                                control={form.control}
+                                error={errors.country}
+                                render={CountryInput}
+                            />
+
+                            <Button type="submit">Submit</Button>
+                        </form>
+                    </Form>
+                </CardContent>
+            </Card>
+        </>
+    );
 }
 
 Create.layout = (page: ReactNode) => (
---
+++ resources/js/Pages/Apartment/Create/CountryInput.tsx
@@ -0,0 +1,122 @@
+import { useState } from "react";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import { PiCaretUpDown } from "react-icons/pi";
+import {
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+} from "@/components/ui/command";
+import { countries } from "@/lib/constants";
+import { MdCheck } from "react-icons/md";
+import * as React from "react";
+import { Apartment, Model } from "@/types";
+import { ControllerRenderProps, Path } from "react-hook-form";
+
+export type FormValues = Omit<
+    Apartment,
+    keyof Model | "host" | "user" | "stars" | "reviews" | "reviews_count"
+>;
+
+interface CountryInputProps {
+    onChange: (value: string) => void;
+    value: string;
+    name: string;
+    innerRef: React.Ref<HTMLInputElement>;
+}
+
+function Input({ onChange, value, name, innerRef }: CountryInputProps) {
+    const [open, setOpen] = useState(false);
+
+    function scrollToSelected() {
+        const element = document.getElementById(value);
+        const container = document.getElementById("countryCommandGroup");
+        if (container && element) {
+            container.scrollTop =
+                element.offsetTop -
+                container.clientHeight / 2 -
+                element.clientHeight / 2;
+        }
+    }
+
+    return (
+        <>
+            <Popover open={open} onOpenChange={(open) => setOpen(open)}>
+                <PopoverTrigger asChild>
+                    <Button
+                        className="w-60 flex justify-between"
+                        variant="outline"
+                    >
+                        {value || "Select Country"}
+
+                        <PiCaretUpDown className="w-4 h-4" />
+                    </Button>
+                </PopoverTrigger>
+                <PopoverContent onOpenAutoFocus={scrollToSelected}>
+                    <Command>
+                        <CommandInput
+                            placeholder="Search country..."
+                            className="h-9"
+                            name={name}
+                        />
+                        <CommandEmpty>No country found</CommandEmpty>
+                        <CommandGroup
+                            className="max-h-80 overflow-y-auto hide-scrollbar"
+                            id="countryCommandGroup"
+                        >
+                            {countries.map((country, index) => (
+                                <CommandItem
+                                    id={country}
+                                    key={index}
+                                    value={country}
+                                    onSelect={() => {
+                                        onChange(country);
+                                        setOpen(false);
+                                    }}
+                                >
+                                    {country}
+                                    {country === value && (
+                                        <MdCheck className="ml-auto w-4 h-4" />
+                                    )}
+                                </CommandItem>
+                            ))}
+                        </CommandGroup>
+                    </Command>
+                </PopoverContent>
+            </Popover>
+
+            <input
+                className="hidden"
+                ref={innerRef}
+                value={value}
+                onChange={(e) => onChange(e.target.value)}
+            />
+        </>
+    );
+}
+
+type ForwardRefProps = React.InputHTMLAttributes<HTMLInputElement> &
+    ControllerRenderProps<FormValues, Path<FormValues>>;
+
+const CountryInput = React.forwardRef<HTMLInputElement, ForwardRefProps>(
+    ({ onChange, value, name, ...props }, ref) => {
+        return (
+            <Input
+                onChange={onChange}
+                value={value as string}
+                name={name}
+                innerRef={ref}
+            />
+        );
+    },
+);
+
+CountryInput.displayName = "CountryInput";
+
+export default CountryInput;
